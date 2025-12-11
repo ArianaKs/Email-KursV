56954753729edea735e5a1090a461d472864548d
ArianaKs <arinaksendzuk02@gmail.com>
2025-12-11 15:01:47 +0300

    Add files via upload

---
+++ EmailSenderAPI-main/EmailSenderAPI/EmailSenderAPI/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Добавляем сервисы
+builder.Services.AddControllers();
+builder.Services.AddHttpClient();
+
+// Добавляем поддержку CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// Используем CORS
+app.UseCors("AllowAll");
+
+// Убираем HTTPS редирект для простоты
+// app.UseHttpsRedirection();
+app.UseAuthorization();
+
+// ВАЖНО: Сначала статические файлы
+app.UseStaticFiles();
+app.UseDefaultFiles();
+
+// Затем контроллеры
+app.MapControllers();
+
+// Главная страница
+app.MapFallbackToFile("index.html");
+
+// Явно указываем порт
+var port = 7200;
+app.Run($"http://localhost:{port}");
